7d642e6f36f94e00d98017876784d0a6709468ba
avi <a0548589558@gmail.com>
2025-11-30 11:59:04 +0200

    Header moved to use a Mui cmponent

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,19 +1,26 @@
-import styles from './Header.module.css';
-import IconButton from "@mui/material/IconButton";
+import { AppBar, Toolbar, Typography, IconButton } from "@mui/material"
+
 import LightModeIcon from "@mui/icons-material/LightMode";
 import DarkModeIcon from "@mui/icons-material/DarkMode";
+
 import { useThemeMode } from "../../context/ThemeContext";
 
 export default function Header() {
   const { mode, toggleMode } = useThemeMode();
 
   return (
-    <div className={styles.headerContainer}>
-      <h1>מערכת לאיסוף אירועים</h1>
+    <AppBar position="fixed">
+      <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
+        
+        <Typography variant="h5" fontWeight="bold">
+          מערכת לאיסוף אירועים
+        </Typography>
 
-      <IconButton onClick={toggleMode} color="inherit">
-        {mode === "light" ? <DarkModeIcon /> : <LightModeIcon />}
-      </IconButton>
-    </div>
+        <IconButton color="inherit" onClick={toggleMode}>
+          {mode === "light" ? <DarkModeIcon /> : <LightModeIcon />}
+        </IconButton>
+
+      </Toolbar>
+    </AppBar>
   );
 }
